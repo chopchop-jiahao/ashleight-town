c3b8978218cf98a958227fc487c715ed4c9528cf
chopchop-jiahao <chopchop.jiahao@gmail.com>
2025-11-07 21:37:37 +0000

    extract correct version

--- ci/extract_version.sh
+++ ci/extract_version.sh
@@ -8,12 +8,12 @@ VERSION_FILE="project.godot"
 # ----------------------------
 # Set regex pattern (macOS compatible)
 # ----------------------------
-VERSION_REGEX='config_version=[0-9A-Za-z.-]+'
+VERSION_REGEX='config/version=[0-9A-Za-z.-]+'
 
 # ----------------------------
 # Extract the version number
 # ----------------------------
-VERSION=$(grep -Eo "$VERSION_REGEX" "$VERSION_FILE" | sed 's/config_version=//')
+VERSION=$(grep -Eo "$VERSION_REGEX" "$VERSION_FILE" | sed 's/config/version=//')
 
 # ----------------------------
 # Check if the version was found
